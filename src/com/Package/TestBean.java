cdbedd310dd7b73868a27098c86620609cd216d7
perwaiz21 <perwaizali2000@gmail.com>
2017-11-27 08:41:07 +0530

    Hi

---
+++ src/com/Package/TestBean.java
@@ -0,0 +1,15 @@
+package com.Package;
+
+public class TestBean {
+
+	private String message = "OOO Hello Developer";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
